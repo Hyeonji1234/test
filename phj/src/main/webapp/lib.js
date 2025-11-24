bcae236cf1e749debf2bae3852a3ff497c42df78
Administrator <Administrator@PC-22>
2025-11-24 10:24:02 +0900

    1

---
+++ phj/src/main/webapp/lib.js
@@ -0,0 +1,6 @@
+function over(obj) {
+	obj.src="https://png.pngtree.com/png-clipart/20220124/original/pngtree-banana-png-image_7196928.png";
+}
+function out(obj){
+	obj.src="https://png.pngtree.com/png-clipart/20190705/original/pngtree-illustration-hand-drawn-red-apple-fruit-material-ai-vector-fruit-element-png-image_4367497.jpg";
+}
